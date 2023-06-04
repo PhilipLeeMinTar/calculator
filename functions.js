3f50b179d90102777e496cb1cbd2ff24c189d1cb
PhilipLeeMinTar <philiplee98@gmail.com>
2023-06-04 22:23:53 +0800

    basic functions okay, done after watching tutorial online

--- functions.js
+++ functions.js
@@ -1,65 +1,52 @@
-const add = function(a,b) {
-    return a+b;
-}
+const equal = document.querySelector(".equal");
+const deleted = document.querySelector(".delete");
+const operators = document.querySelectorAll(".operator");
+const prevDisplay = document.querySelector(".display-previous");
+const curDisplay = document.querySelector(".display-current");
+const digitButtons = document.querySelectorAll(".digits");
 
-const subtract = function(a,b) {
-    return a-b;
-}
-
-const multiply = function(a,b) {
-    return a * b;
-}
-
-const divide = function(a,b){
-    return a/b;
-}
-
-let currentNumber = 0;  //need to get user's input prompt()
+let currentNumber = "";  //need to get user's input prompt()
 let operator = "*"
-let previousNumber = null;
-
-const operate = (first, second, operator) => {
-    //use the operators from above;
-}
-
-const displayValue = () => {
-    let display = document.querySelector(".display-current");
-    let digitButtons = document.querySelectorAll(".digits");
-    digitButtons.forEach((button) => {
-        button.addEventListener("click", (event) => {
-            // event.target.style.background = 'blue';
-            if (display.textContent === "0") display.textContent = "";
-            display.textContent += event.target.textContent;
-        })
-    });
-
-    let value;
-    let inputOperator;
-    let operators = document.querySelectorAll('.operators');
-    operators.forEach((operator)=>{
-        operator.addEventListener("click", (event) => {
-            value = parseInt(display.textContent);
-            inputOperator = event.target.textContent
-        })
-    })
-    
-
-    // display.textContent += lol;
-}
-
-let operators = document.querySelectorAll('.operators');
-operators.forEach((operator)=>{
-    operator.addEventListener("click", (event)=>{
-        currentNumber = parseFloat(display.textContent);
-        switch(operator.textContent) {
-            case '+': previousNumber = add(previousNumber, currentNumber); break; //need to do this before the 2nd number
-            case '-': previousNumber = subtract(previousNumber, currentNumber); break;
-            case '×': multiply(previousNumber, currentNumber); break;
-            case '÷': divide(previousNumber, currentNumber); break;
-        }
-        
+let previousNumber = 0;
+// let pressOperator = false;
+digitButtons.forEach(digit => {
+    digit.addEventListener('click', (event)=>{
+        displayValue(event.target.textContent);
+        // operate();
     })
 })
 
+function displayValue(numberString) {
+    currentNumber += numberString;
+    if (currentNumber === ""){
+        curDisplay.textContent = "0";
+        return;
+    }
+    curDisplay.textContent = currentNumber;
+    // console.log(currentNumber);
+}
 
-let firstValue = displayValue()
+operators.forEach(operator => {
+    operator.addEventListener('click', (event)=>{
+        previousNumber = currentNumber;
+        prevDisplay.textContent = previousNumber + " " + operator.textContent;
+        curDisplay.textContent = "0"
+        currentNumber = "";
+        op = operator.textContent;
+    })
+})
+
+equal.addEventListener('click', operate);
+
+function operate(){
+    let result = "";
+    
+    switch(op) {
+        case '+': result = Number(previousNumber) + Number(currentNumber); break; //need to do this before the 2nd number
+        case '-': result = Number(previousNumber) - Number(currentNumber); break;
+        case '×': result = Number(previousNumber) * Number(currentNumber); break;
+        case '÷': result = Number(previousNumber) / Number(currentNumber); break;
+    }
+    currentNumber = result.toString();
+    curDisplay.textContent = currentNumber;
+}
